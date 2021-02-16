d7b7aa16c2f570bd01d7e74bf31735b8bdd16fc9
tonyachi <68254832+tonyachi@users.noreply.github.com>
2021-02-16 15:37:53 -0500

    WIP on navigation

--- React/react-website/src/components/Navbar.js
+++ React/react-website/src/components/Navbar.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from './Button';
-import './Navbar.css'
+import './Navbar.css';
 import { Link } from 'react-router-dom';
 import { FaTypo3 } from 'react-icons/fa';
 import { GoThreeBars } from "react-icons/go";
-import { GrClose } from 'react-icons/gr'
+import { MdClose } from 'react-icons/md';
+
 
 function Navbar() {
     const [click, setClick] = useState(false);
@@ -15,44 +16,57 @@ function Navbar() {
     const closeMobileMenu = () => setClick(false);
 
     const showButton = () => {
-        if (window.innerWidth <= 900) {
+        if (window.innerWidth <= 960) {
             setButton(false)
         } else {
             setButton(true)
         }
     }
+    useEffect(() => {
+        showButton()
+    }, [])
 
     window.addEventListener('resize', showButton)
-
-    const menu = [
-        { name: 'Home', link: 'home' },
-        { name: 'Services', link: 'services' },
-        { name: 'Portfolio', link: 'portfolio' },
-        { name: 'Sign-In', link: 'sign-in' },
-    ]
 
     return (
         <>
             <nav className="navbar">
                 <div className="navbar-container">
-                    <Link to="/" className="navbar-logo">TRVL <FaTypo3 className='fa-typo3' /></Link>
+                    <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>TRVL <FaTypo3 className='fa-typo3' /></Link>
                 </div>
                 <div className="menu-icon" onClick={handleClick}>
-                    {click ? <GrClose /> : <GoThreeBars />}
+                    {click ? <MdClose /> : <GoThreeBars />}
                 </div>
                 <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                    {menu.map((item) => {
-                        return <li className='nav-item'>
-                            <Link
-                                to='/'
-                                className='nav-links {item.link}'
-                                onClick={closeMobileMenu}>
-                                {item.name}
-                            </Link>
-                        </li>
-                    })}
+                    <li className='nav-item'>
+                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                            Home
+              </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link
+                            to='/services'
+                            className='nav-links'
+                            onClick={closeMobileMenu}
+                        >Services</Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link
+                            to='/products'
+                            className='nav-links'
+                            onClick={closeMobileMenu}
+                        >Products</Link>
+                    </li>
+
+                    <li>
+                        <Link
+                            to='/sign-up'
+                            className='nav-links-mobile'
+                            onClick={closeMobileMenu}
+                        >Sign Up</Link>
+                    </li>
                 </ul>
-                {button && <Button buttonStyle='btn--outline'>Sign Up</Button>}
+                {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
             </nav>
         </>
     )
