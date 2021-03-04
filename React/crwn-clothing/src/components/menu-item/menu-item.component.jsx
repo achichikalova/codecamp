fa984195d7e66473ed875caff2309c238e7dfc3a
tonyachi <68254832+tonyachi@users.noreply.github.com>
2021-03-04 12:37:01 -0500

    update our menu-items to navigate withRouter HOC

--- React/crwn-clothing/src/components/menu-item/menu-item.component.jsx
+++ React/crwn-clothing/src/components/menu-item/menu-item.component.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
-import './menu-item.style.scss'
+import React from "react";
+import { withRouter } from "react-router-dom";
 
-export const MenuItem = ({title, image, size}) => {
-    return (
-        <div className={`${size} menu-item`}>
-            <div className='background-image' style={{backgroundImage: `url("${image}")`}} />
-            <div className='content'>
-                <h1 className='title'>{title.toUpperCase()}</h1>
-                <span className='subtitle'>SHOP NOW</span>
-            </div>
-        </div>
-    )
-}
+import "./menu-item.style.scss";
+
+const MenuItem = ({ title, imageUrl, size, history, match }) => {
+  return (
+    <div
+      className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${title}`)}
+    >
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url("${imageUrl}")` }}
+      />
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
+
+export default withRouter(MenuItem);
