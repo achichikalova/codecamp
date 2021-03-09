246d0ad2d490e4274697e851dea732f91be2fd70
tonyachi <68254832+tonyachi@users.noreply.github.com>
2021-03-09 08:38:47 -0500

    work on collection item component, finished collection preview component

---
+++ React/crwn-clothing/src/pages/homepage/nomepage.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Directory from "../../components/directory/directiry.component";
+import "../homepage/homepage.style.scss";
+
+export const HomePage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
