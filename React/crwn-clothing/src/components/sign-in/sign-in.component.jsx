fda7bb6366d2f77529b510ca52b90851a1c930f5
tonyachi <68254832+tonyachi@users.noreply.github.com>
2021-03-16 11:40:47 -0400

    add form-input component and style

--- React/crwn-clothing/src/components/sign-in/sign-in.component.jsx
+++ React/crwn-clothing/src/components/sign-in/sign-in.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import FormInput from "../form-input/form-input.component";
 import "./sign-in.style.scss";
 
 class SignIn extends React.Component {
@@ -17,7 +18,7 @@ class SignIn extends React.Component {
     this.setState({ email: "", password: "" });
   };
 
-  handleSubmit = (event) => {
+  handleChange = (event) => {
     const { value, name } = event.target;
 
     this.setState({ [name]: value });
@@ -29,21 +30,23 @@ class SignIn extends React.Component {
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
 
-        <form omSubmit={this.handleSubmit}>
-          <input
+        <form handleSubmit={this.handleSubmit}>
+          <FormInput
             type="email"
             name="email"
+            handleChange={this.handleChange}
             value={this.state.email}
+            label='Email'
             required />
-          <label>Email</label>
 
-          <input
+          <FormInput
             type="password"
             name="password"
+            handleChange={this.handleChange}
             value={this.state.password}
+            label='Password'
             required
           />
-          <label>Password</label>
 
           <input type="submit" value="Submit Form" />
         </form>
