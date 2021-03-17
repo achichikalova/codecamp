f3d83231f30f83ff90afb12f6dddb22a6c5d0541
tonyachi <68254832+tonyachi@users.noreply.github.com>
2021-03-17 11:26:10 -0400

    added custom button component

---
+++ React/crwn-clothing/src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyB-kzuGVmlJ8jY1PgxZQ5d8SvaqLfJYuJw",
+    authDomain: "crwn-db-1acaa.firebaseapp.com",
+    projectId: "crwn-db-1acaa",
+    storageBucket: "crwn-db-1acaa.appspot.com",
+    messagingSenderId: "579399205199",
+    appId: "1:579399205199:web:443d0b7ca82c3d9ffec53a",
+    measurementId: "G-LX3FL0DECR"
+  }
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
