d5cb72275f748e9fcaa9b8ce219a64404ccabe52
tonyachi <68254832+tonyachi@users.noreply.github.com>
2021-07-08 16:18:23 -0400

    returning to the project

--- React/crwn-clothing/src/App.js
+++ React/crwn-clothing/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/header/header.component";
-import HomePage from "./pages/homepage/nomepage.component";
+import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import { auth } from './firebase/firebase.utils';
@@ -21,6 +21,7 @@ class App extends React.Component {
     componentDidMount() {
         this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
             this.setState({ currentUser: user });
+            console.log(user)
         })
     }
 
